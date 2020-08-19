ecba79a347b04f319e154dbc4cb775a409442420
MuriloCicolin <mu_cicolin@hotmail.com>
2020-08-18 22:46:20 -0300

    Finalizado layout pagina Dashboard

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,36 +1,50 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
+import { useAuth } from '../../context/AuthContext';
+
+import bgImage from '../../assets/home.png';
 
 import {
   Container,
   HeaderContent,
   Image,
-  LogOffIcon,
+  ButtonIcon,
   BackgroundImage,
+  ContainerButton,
   RegisterButton,
   ListButton,
 } from './styles';
 
 const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
+
   return (
     <Container>
       <HeaderContent>
-        <Image src="https://avatars1.githubusercontent.com/u/49662901?v=4" />
-        <LogOffIcon />
+        <div>
+          <Image src="https://avatars1.githubusercontent.com/u/49662901?v=4" />
+          <h2>Murilo Cicolin</h2>
+        </div>
+        <ButtonIcon type="button" onClick={signOut}>
+          <FiPower size={25} />
+        </ButtonIcon>
       </HeaderContent>
 
-      <BackgroundImage />
+      <BackgroundImage src={bgImage} />
 
       <h1>
-        Seja Bem-vindo, <strong>O que deseja fazer ?</strong>
+        Seja Bem-vindo, <br /> O que deseja fazer ?
       </h1>
 
-      <RegisterButton>
-        <span>Cadastra Imóveis</span>
-      </RegisterButton>
+      <ContainerButton>
+        <RegisterButton>
+          <span>Cadastrar Imóveis</span>
+        </RegisterButton>
 
-      <ListButton>
-        <span>Procurar Imóveis Imóveis</span>
-      </ListButton>
+        <ListButton>
+          <span>Procurar Imóveis</span>
+        </ListButton>
+      </ContainerButton>
 
       <span>Total de 250 móveis cadastrados</span>
     </Container>
--- web/src/pages/Dashboard/styles.ts
+++ web/src/pages/Dashboard/styles.ts
@@ -1,15 +1,132 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.section`
+  width: 100vw;
+  height: 100%;
+  background: #ddf3f5;
 
-export const HeaderContent = styled.div``;
+  > h1 {
+    color: #1b6ca8;
+    display: flex;
+    justify-content: center;
+  }
 
-export const Image = styled.img``;
+  > span {
+    color: #1b6ca8;
+    display: block;
+    text-align: center;
+    padding-top: 2rem;
+    font-size: 1.8rem;
+    background: #ddf3f5;
+  }
 
-export const LogOffIcon = styled.div``;
+  @media (min-width: 1100px) {
+    > span {
+      padding-top: 2.5rem;
+    }
+  }
+`;
 
-export const BackgroundImage = styled.div``;
+export const HeaderContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 2rem 2rem 2rem 2rem;
 
-export const RegisterButton = styled.button``;
+  > div {
+    display: flex;
+    align-items: center;
 
-export const ListButton = styled.button``;
+    h2 {
+      margin-left: 2rem;
+    }
+  }
+`;
+
+export const Image = styled.img`
+  width: 6rem;
+  height: 6rem;
+  border-radius: 50%;
+`;
+
+export const ButtonIcon = styled.button`
+  outline: 0;
+  border: 0;
+  margin-right: 3rem;
+  cursor: pointer;
+`;
+
+export const BackgroundImage = styled.img`
+  width: 32rem;
+  height: 32rem;
+  display: block;
+  margin: 0 auto;
+
+  @media (min-width: 1100px) {
+    width: 45rem;
+    height: 45rem;
+  }
+`;
+
+export const ContainerButton = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 2rem;
+
+  @media (min-width: 1100px) {
+    flex-direction: row;
+    align-items: center;
+    margin: 0 auto;
+    max-width: 700px;
+  }
+`;
+
+export const RegisterButton = styled.button`
+  border: 0;
+  background: #0a97b0;
+  padding: 2.5rem;
+  width: 30rem;
+  border-radius: 0.8rem;
+  margin: 0 auto;
+  cursor: pointer;
+  outline: 0;
+  color: #ffffff;
+  font-weight: 500;
+  font-size: 2rem;
+  transition: 0.2 background-color;
+
+  &:hover {
+    background: ${shade(0.2, '#0a97b0')};
+  }
+
+  @media (min-width: 1100px) {
+    margin-top: 3rem;
+  }
+`;
+
+export const ListButton = styled.button`
+  border: 0;
+  background: #1b6ca8;
+  padding: 2.5rem;
+  width: 30rem;
+  border-radius: 0.8rem;
+  outline: 0;
+  margin: 0 auto;
+  cursor: pointer;
+  margin-top: 2rem;
+
+  color: #ffffff;
+  font-weight: 500;
+  font-size: 2rem;
+
+  transition: 0.2 background-color;
+
+  &:hover {
+    background: ${shade(0.2, '#1b6ca8')};
+  }
+
+  @media (min-width: 1100px) {
+    margin-top: 3rem;
+  }
+`;
